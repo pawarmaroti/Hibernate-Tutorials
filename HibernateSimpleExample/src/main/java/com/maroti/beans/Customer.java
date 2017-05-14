0e53dc554f8e142c875ff363cd1225ffb4bf01a0
pawarmaroti <marotiwpawar@gmail.com>
2017-05-15 00:35:56 +0530

    basic example

---
+++ HibernateSimpleExample/src/main/java/com/maroti/beans/Customer.java
@@ -0,0 +1,28 @@
+package com.maroti.beans;
+
+public class Customer {
+	private int id;
+	private String name;
+	private String addr;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	
+
+}
---
+++ HibernateSimpleExample/src/main/java/com/maroti/client/TestHibernate.java
@@ -0,0 +1,30 @@
+package com.maroti.client;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.maroti.beans.Customer;
+
+public class TestHibernate {
+	public static void main(String[] args) {
+		
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory sf=cfg.buildSessionFactory();
+		Session s=sf.openSession();
+		
+		Transaction tx=s.beginTransaction();
+		Customer cst = new Customer();
+		cst.setId(101);
+		cst.setName("Maroti");
+		cst.setAddr("Nanded");
+		s.save(cst);
+		tx.commit();
+		System.out.println("Success!!!!!!!!!!!!!!!!!!!!");
+		s.close();
+		
+	}
+
+}
